b5e1ca1d3af2ede3c011cc4890eb7176c5d20a0e
ClintonYounge <clintonyounge18@gmail.com>
2023-03-22 01:30:33 -0400

    Update project setup
    - Update the html content to match the provided structure.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+
+const scores = require('./modules/scores.js');
+
+const scoreList = document.querySelector('.score-list');
+
+let output = '';
+
+scores.forEach((score) => {
+  output += `<li>${score.name}: ${score.score}</li><hr>`;
+});
+
+scoreList.innerHTML = output;
